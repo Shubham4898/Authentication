a9cb44e07429bfae062bd39e48c3e48c36ca330e
shubham <skm4898@gmail.com>
2021-05-01 11:27:02 +0530

    Implemented route for updating user info

--- app.js
+++ app.js
@@ -43,6 +43,39 @@ app.get('/profiles',(req,res)=>{
 
 })
 
+app.put('/profile',(req,res) => {
+   let isValid = false;
+   let indexarray = -1;
+
+   for(let i=0;i<userData.length;i++){
+      const currentUser = userData[i].username;
+      const currentPass = userData[i].password;
+      if(req.body.username === currentUser && req.body.password === currentPass){
+         isValid = true;
+         indexarray = i;
+         break;
+      }
+      
+      if(!isValid){
+         res.status(401).json({message : "Invalid username or password"});
+      }
+      else{
+
+         userData[i].name = req.body.name;
+         userData[i].college = req.body.college
+         res.status(201).json({message : "Updated successfully"})
+
+      }
+
+
+   }
+   
+   
+
+
+
+})
+
 console.log(userData);
 app.listen(7050,()=>{
    console.log('app is working');
